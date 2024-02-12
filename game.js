f97881f189dfc4cf5674fcc7d315f39745775e54
saandre0217 <s.a.andre0217@gmail.com>
2024-02-12 13:43:24 -0600

    bug fixed when player enters number not on board; logic for first play complete;

--- game.js
+++ game.js
@@ -1,26 +1,69 @@
-var rls = require('readline-sync');
+const rls = require('readline-sync');
+
 
 let playerName; 
 let playerLetter;
-let board = [1, 2, 3, 'X', 5, 6, 7, 8, 9]
+const emptyBoard = [1, 2, 3, 'X', 5, 6, 7, 8, 9]
 let playerMove;
+let playerMoves = []
+let computerLetter;
 const printBoard = (boardArr) => {
- console.log(` ${boardArr[0]} | ${boardArr[1]} | ${boardArr[2]} \n --------- \n ${boardArr[3]} | ${boardArr[4]} | ${boardArr[5]} \n --------- \n ${boardArr[6]} | ${boardArr[7]} | ${boardArr[8]}`)
+ console.log(`\n ${boardArr[0]} | ${boardArr[1]} | ${boardArr[2]} \n --------- \n ${boardArr[3]} | ${boardArr[4]} | ${boardArr[5]} \n --------- \n ${boardArr[6]} | ${boardArr[7]} | ${boardArr[8]}`)
 }
 
-const handleComputerMove = (currBoard) => {
-
+const updateBoard = (letter, index, currentBoard) => {
+  const newBoard = currentBoard.slice()
+  newBoard.splice(index, 1, letter)
+  return newBoard;
 }
-const handlePlayerMove = (move, prevBoard) => {
-if(move > 9 || move < 1 || !prevBoard.includes(move)){
-  const newMove = rls.question(`Oops! ${move} isn't on the board. Try again!`)
-  console.log(prevBoard, newMove)
-  return handlePlayerMove(newMove, prevBoard)
+
+const loadingAnimation = () => {
+  let x = 0;
+  chars = ["⠙", "⠘", "⠰", "⠴", "⠤", "⠦", "⠆", "⠃", "⠋", "⠉"];
+  return setInterval(function() {
+      process.stdout.write(`\r ${chars[x++]} Trixie is Thinking \r`);
+      x = x % chars.length;
+  }, 100);
+}
+
+const beatPlayer = (currentBoard) => {
+  let computerMoveInd;
+  const computerMoves = [
+    [1, 2, 3, 6, 4, 8],
+    [0, 2, 4, 7],
+    [0, 1, 4, 6, 5, 8],
+    [0, 6, 4, 5],
+    [0, 8, 1, 7, 2, 6, 3, 5],
+    [2, 8, 4, 3],
+    [0, 3, 7, 8, 2, 4],
+    [1, 4, 6, 8],
+    [0, 4, 2, 5, 6, 7]
+  ]
+if(playerMoves.length === 1){
+  const playerMoveInd = playerMoves[0]
+  const randomMoveInd = Math.floor(Math.random() * computerMoves[playerMoveInd].length)
+  computerMoveInd = computerMoves[playerMoveInd][randomMoveInd]
+  return updateBoard(computerLetter, computerMoveInd, currentBoard)
+}
+}
+const handlePlayerMove = (prevBoard) => {
+  playerMove = rls.questionInt(`Your Turn!`)
+if(playerMove > 9 || playerMove < 1 || !prevBoard.includes(playerMove)){
+  console.log(`Oops! ${playerMove} isn't on the board. Try again!`)
+  return handlePlayerMove(prevBoard)
 } else {
-const moveInd = move - 1;
-const boardWPlayerMove = prevBoard.slice()
-boardWPlayerMove.splice(moveInd, 1, playerLetter)
+const moveInd = playerMove - 1;
+playerMoves.push(moveInd)
+const boardWPlayerMove = updateBoard(playerLetter, moveInd, prevBoard)
 printBoard(boardWPlayerMove)
+console.log(`Nice Choice! Let's see where Tic Tac Toe Wiz, Trixie, plays her ${computerLetter}`)
+let loading = loadingAnimation()
+setTimeout(() => {
+  clearInterval(loading)
+  const boardWComputerMove = beatPlayer(boardWPlayerMove)
+  printBoard(boardWComputerMove)
+  
+}, 2000)
 }
 }
 //game play
@@ -28,10 +71,13 @@ printBoard(boardWPlayerMove)
 
   if (rls.keyInYN(`Excited to play, ${playerName}?! Enter Y if you want to play as X's or N if you want to play as O's` )){
     playerLetter='X'
+    computerLetter='O'
   } else {
     playerLetter='O'
+    computerLetter='X'
   }
-  printBoard(board)
-  playerMove = rls.questionInt(`Great! Let\'s get started. You go first! Enter the number where you want your first ${playerLetter} to go!`)
-  handlePlayerMove(playerMove, board)
+  printBoard(emptyBoard)
+  console.log(`Great! Let's get started. \nYou will be playing against Tic Tac Toe Wiz, Trixie. \nWhen it is your turn, you will enter the number where you want your ${playerLetter} to go!`)
+
+  handlePlayerMove(emptyBoard)
 
